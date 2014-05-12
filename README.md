9149935987d9de1e0131e3de0240f142a5c9f26b
cliff777 <bugman12345@gmail.com>
2014-05-12 11:04:46 -0400

    Server class and link in readme

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ SeniorProjectServer
 ===================
 
 Server for our senior Project
+
+Game project: https://github.com/will12525/SeniorProject
---
+++ src/server/Server.java
@@ -0,0 +1,18 @@
+package server;
+
+public class Server 
+{
+	/*
+	 * Main class for the server
+	 * This class will accept incoming connections, 
+	 * create client objects, and initialize the
+	 * game level 
+	 */
+	
+	public static void main(String[] args)
+	{
+		
+	}
+	
+
+}
